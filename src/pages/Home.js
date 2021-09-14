f3487dac5a6931baa3694c27ea977675dc9e3ec5
Thiago Torres de Souza <thiagomev@gmail.com>
2021-09-14 19:04:39 -0300

    Add useEffect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -6,22 +6,43 @@ import {
   TextInput,
   Platform,
   ScrollView,
-  FlatList
+  FlatList,
 } from 'react-native';
+
 import { Button } from '../components/Button';
 import { SkillCard } from '../components/SkillCard';
 
 export function Home() {
   const [newSkill, setNewSkill] = useState("");
   const [mySkills, setMySkills] = useState([]);
+  const [gretting, setGretting] = useState("");
 
   function handleAddNewSkill() {
     setMySkills(oldState => [...oldState, newSkill]);
   }
 
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12) {
+      setGretting('Good Morning');
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGretting('Good Afternoon');
+    } else {
+      setGretting('Good Night');
+    }
+
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Welcome, Thiago</Text>
+      <Text style={styles.title}>
+        Welcome, Thiago
+      </Text>
+
+      <Text style={styles.greetings}>
+        {gretting}
+      </Text>
 
       <TextInput
         style={styles.input}
@@ -41,11 +62,10 @@ export function Home() {
       <FlatList
         data={mySkills}
         keyExtractor={item => item}
-        renderItem={({ item }) =>(
+        renderItem={({ item }) => (
           <SkillCard skill={item} />
         )}
       />
-
     </View>
   );
 }
@@ -70,5 +90,8 @@ const styles = StyleSheet.create({
     padding: Platform.OS === 'ios' ? 15 : 10,
     marginTop: 30,
     borderRadius: 7,
+  },
+  greetings: {
+    color: '#FFF',
   }
 })
